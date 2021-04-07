a2255c5462e6b58e9bd3e3996d1a83fc49b7881e
Thiago Fontanella <thiago.fontanella@hotmail.com>
2021-04-07 17:15:05 -0300

    projeto finalizado

---
+++ projeto_inicial/examples.js
@@ -0,0 +1,7 @@
+// document.getElementById(‘id’) seleciona o elemento pelo id passado.
+// document.getElementsByClassName(‘classe’) retorna um array dos elementos pelo nome da classe passada.
+// document.getElementsByTagName(‘tag’) retorna um array dos elementos pelo nome da tag passada
+// document.querySelectorAll(seletor) devolve todos os seletores com o mesmo nome
+// insertBefore(pai, filho): Coloca um nó antes do outro.
+// replaceChild( elemento1, elemento2): Substitui o nó elemento 1 pelo nó elemento2.
+// removeChild(elemento): Remove um nó da árvore.
